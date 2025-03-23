9ac94fc0b568162a46fc4e565aea4f74656d05de
Daniel H <daniel.neves0021@gmail.com>
2025-03-23 13:46:15 -0300

    feat: matrix logic complete

--- GaussJordanSolver/GaussJordanSolver.cs
+++ GaussJordanSolver/GaussJordanSolver.cs
@@ -1,109 +1,105 @@
-using System.ComponentModel.Design;
-using System.IO.IsolatedStorage;
-using System.Runtime.InteropServices;
-
-namespace GaussJordanSolverPackage; 
-
-class GaussJordanSolver 
+namespace GaussJordanSolverPackage;
+class GaussJordanSolver
 {
-    public GaussJordanSolver() {}
-    public double[] Solve(double[][] matrix) 
+    public GaussJordanSolver() { }
+    public double[] Solve(double[,] matrix)
     {
-        double[] solvedArray = new double[matrix.Length];
-        if (!IsSingular(CalculateDeterminant(matrix))) 
+        double[] solvedArray = new double[matrix.GetLength(0)];
+        if (!IsSingular(CalculateDeterminant(matrix)))
         {
-            for (int i = 0; i < matrix.Length; i++) 
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                if (matrix[i][i] == 0)
+                if (matrix[i, i] == 0)
                 {
-                    for (int j = 0; j < matrix[i].Length - 1; j++)
+                    for (int j = 0; j < matrix.GetLength(1); j++)
                     {
-                        if (matrix[j][i] != 0)
-                            SwapMatrix(matrix, i, j);
-                        else throw new Exception("Singular Matrix");
-
+                        //if (matrix[j, i] != 0)
+                        //    SwapMatrix(matrix, i, j);
+                        //else throw new Exception("Singular Matrix");
+                        continue;
                     }
                 }
                 // Ln < -Ln / matrix[i][i]
-                if (matrix[i][i] != 1) 
+                if (matrix[i, i] != 1)
                 {
-                    double coef = matrix[i][i];
-                    for (int j = 0; j < matrix[i].Length - 1; j++)
-                        matrix[i][j] /= coef;
+                    double coef = matrix[i, i];
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                        matrix[i, j] /= coef;
                 }
                 // Ln <- Ln - matrix[i][j]*Ln
-                for (int j = 0; j < matrix.Length - 1; j++) 
+                for (int j = 0; j < matrix.GetLength(0); j++)
                 {
                     if (i != j)
                     {
-                        double coef = matrix[j][i];
-                        for (int k = 0; k < matrix[i].Length - 1; k++)
+                        double coef = matrix[j, i];
+                        for (int k = 0; k < matrix.GetLength(1); k++)
                         {
-                            matrix[j][k] -= coef * matrix[i][k];
+                            matrix[j, k] -= coef * matrix[i, k];
                         }
                     }
                 }
             }
-            for (int j = 0; j < matrix.Length ; j++)
-                solvedArray[j] = matrix[j][matrix.Length];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                solvedArray[i] = matrix[i, matrix.GetLength(1) - 1];
         }
-        printSolveMatrix(matrix);
+        //printSolveMatrix(matrix);
         return solvedArray;
     }
-    public void printSolveMatrix(double[][] matrix)
+    public void printSolveMatrix(double[,] matrix)
     {
-        for (int i = 0; i < matrix.Length; i++) 
+        for (int i = 0; i < matrix.GetLength(0); i++)
         {
             Console.Write(" | ");
-            for (int j = 0; j < matrix[i].Length; j++)
+            for (int j = 0; j < matrix.GetLength(1); j++)
             {
-                Console.Write($"{matrix[i][j]}, ");
+                Console.Write($"{matrix[i, j]}, ");
             }
-            Console.WriteLine(" | ");        
+            Console.WriteLine(" | ");
         }
-
     }
-
-    public void printSolve(double[] matrix)
+    public void printSolve(double[] solvedMatrix)
     {
+        Console.WriteLine("Rounded Solution:");
         Console.Write("| ");
-        for (int i = 0; i < matrix.Length; i++)
-            Console.Write($"{matrix[i]}, ");
+        for (int i = 0; i < solvedMatrix.Length; i++)
+            Console.Write($"{Math.Round(solvedMatrix[i])}, ");
         Console.Write(" |");
 
     }
-    private double CalculateDeterminant(double[][] matrix)  
+    private double CalculateDeterminant(double[,] matrix)
     {
-        double[][] _matrix = matrix;
+        double[,] _matrix = new double[matrix.GetLength(0), matrix.GetLength(1)];
+        Array.Copy(matrix, _matrix, matrix.Length);
         double determinant = 1.0;
-        for (int i = 0; i < _matrix.Length; i++)
+        for (int i = 0; i < _matrix.GetLength(0); i++)
         {
-            if (_matrix[i][i] == 0)
+            if (_matrix[i, i] == 0)
             {
-                for (int j = 1; j < _matrix.Length; j++)
+                for (int j = 1; j < _matrix.GetLength(1); j++)
                 {
-                    if (_matrix[j][i] != 0)
-                    {
-                        double[] mainBucket = _matrix[j];
-                        SwapMatrix(_matrix, i, j);
-                    }
-                    else throw new Exception("Singular Matrix");
+                    //if (_matrix[j,i] != 0)
+                    //{
+                    //    double[] mainBucket = _matrix[j];
+                    //    SwapMatrix(_matrix, i, j);
+                    //}
+                    //else throw new Exception("Singular Matrix");
+                    continue;
                 }
             }
-            for (int j = i+1; j < _matrix.Length; j ++)
+            for (int j = i + 1; j < _matrix.GetLength(0); j++)
             {
-                double coef = _matrix[j][i] / _matrix[i][i];
-                for (int k = 0; k < _matrix[j].Length;)
+                double coef = _matrix[j, i] / _matrix[i, i];
+                for (int k = 0; k < _matrix.GetLength(1);)
                 {
-                    for (int l = 0; l < _matrix[j].Length; l++)
-                    _matrix[j][l] = _matrix[j][l] - coef * _matrix[i][l];
+                    for (int l = 0; l < _matrix.GetLength(1); l++)
+                        _matrix[j, l] = _matrix[j, l] - coef * _matrix[i, l];
                     break;
                 }
             }
         }
-        for (int i = 0; i< _matrix.Length; i++)
-            determinant *= _matrix[i][i];
-        return determinant; 
+        for (int i = 0; i < _matrix.GetLength(0); i++)
+            determinant *= _matrix[i, i];
+        return determinant;
     }
     private bool IsSingular(double determinant)
     {
@@ -111,12 +107,11 @@ class GaussJordanSolver
             return false;
         return true;
     }
-    private double[][] SwapMatrix(double[][] matrix, int iIndex, int jIndex) 
+    private double[][] SwapMatrix(double[][] matrix, int iIndex, int jIndex)
     {
         double[] bucket = matrix[iIndex];
         matrix[iIndex] = matrix[jIndex];
         matrix[jIndex] = bucket;
         return matrix;
     }
-
 }
--- GaussJordanSolver/Program.cs
+++ GaussJordanSolver/Program.cs
@@ -4,11 +4,11 @@ public class Program
     public static void Main(String[] args)
     {
         GaussJordanSolver g = new GaussJordanSolver();
-        double[][] matrix =
+        double[,] matrix =
         {
-                [ 3.0,  2.0, -4.0, 3.0 ],
-                [ 2.0,  3.0,  3.0, 15.0 ],
-                [5.0, -3.0,  1.0, 14.0 ],
+                { 3.0,  -4.0, -4.0 },
+                { 2.0,  3.0,  3.0},
+
         };
         g.printSolve(g.Solve(matrix));
     }
